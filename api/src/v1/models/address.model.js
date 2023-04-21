d90ea042cbea1eaa78ea7bad7ec66c626dc56d0a
neeswebservices <neeswebservice@gmail.com>
2023-04-21 23:24:51 +0545

    init

--- api/src/v1/models/address.model.js
+++ api/src/v1/models/address.model.js
@@ -1,14 +1,14 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const addressSchema = new mongoose.Schema({
-	street: String,
-	postalCode: String,
-	city: String,
-	country: String,
-	location: String,
-    phone: String
-})
+  street: String,
+  postalCode: String,
+  city: String,
+  country: String,
+  location: String,
+  phone: String,
+});
 
-const Address = mongoose.model("Address", addressSchema)
+const Address = mongoose.model("Address", addressSchema);
 
-export default Address
+export default Address;
