ee1fc36451a26a0f99e97ae3672ef8dbbdd9ae3f
neeswebservices <neeswebservice@gmail.com>
2023-04-24 19:59:50 +0545

    11

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,37 +1,68 @@
+import Select from "react-select";
+import { useQuery } from "@tanstack/react-query";
+import getAPI from "../api/getApi.js";
+import Spinner from "./common/Spinner";
+
 export const Hero = () => {
-    return (
-        <div className="bg-white ml-6 mt-[-100px]">
-            <div className="flex items-center max-h-[100vh] h-screen">
-                <div className=" px-8 w-1/2">
-                    <div className="text-7xl text-gray-900 font-semibold">
-                        <h1>Building digital</h1>
-                        <h1>products, brands</h1>
-                        <h1 className="text-blue-600">experience</h1>
-                    </div>
-                    <div className="mt-4">
-                        <p className="text-gray-700">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad
-                            similique nam fugit veritatis quos expedita quidem maxime at quas
-                            voluptatum, architecto maiores excepturi. Quos quia, magnam
-                            accusamus quasi impedit necessitatibus.
-                        </p>
-                    </div>
-                    <div className="mt-4 flex items-center space-x-4">
-                        <input
-                            type="text"
-                            className="bg-white rounded h-full py-2.5 w-3/5 border border-gray-400/50 px-2"
-                            placeholder="Enter you email"
-                        />
-                        <button className="rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white">
-                            Submit
-                        </button>
-                    </div>
-                </div>
+  const { data, isLoading, error, refetch } = useQuery(["branch"], () =>
+    getAPI.getBranches()
+  );
+  const options = data
+    ? data.data.reduce((acc, cur) => {
+        acc.push({
+          value: `${cur.city}, ${cur.address}`,
+          label: cur.name,
+          ...cur,
+        });
+        return acc;
+      }, [])
+    : [];
 
-
+  return (
+    <div className="bg-white ml-6 mt-[-100px]">
+      <div className="flex items-center max-h-[100vh] h-screen">
+        <div className=" px-8 w-1/2">
+          <div className="text-7xl text-gray-900 font-semibold">
+            <h1>Building digital</h1>
+            <h1>products, brands</h1>
+            <h1 className="text-blue-600">experience</h1>
+          </div>
+          <div className="mt-4">
+            <p className="text-gray-700">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad
+              similique nam fugit veritatis quos expedita quidem maxime at quas
+              voluptatum, architecto maiores excepturi. Quos quia, magnam
+              accusamus quasi impedit necessitatibus.
+            </p>
+          </div>
+          {isLoading ? (
+            <Spinner />
+          ) : (
+            <div className="mt-4 flex items-center space-x-4">
+              <Select
+                classNamePrefix="select"
+                styles={{
+                  control: (baseStyles, state) => ({
+                    ...baseStyles,
+                    borderColor: state.isFocused ? "blue" : "grey",
+                    width: "300px",
+                  }),
+                }}
+                defaultValue={options[0]}
+                isSearchable={true}
+                name="branch"
+                options={options}
+                placeholder="Select the branch..."
+              />
+              <button className="rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white">
+                Submit
+              </button>
             </div>
+          )}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
-export default Hero; 
+export default Hero;
--- client/src/pages/Admin/components/shared/Sidebar.jsx
+++ client/src/pages/Admin/components/shared/Sidebar.jsx
@@ -1,45 +1,53 @@
-import React from 'react'
-import classNames from 'classnames'
-import { Link, useLocation } from 'react-router-dom'
-import { HiOutlineLogout } from 'react-icons/hi'
-import { DASHBOARD_SIDEBAR_LINKS, DASHBOARD_SIDEBAR_BOTTOM_LINKS } from '../../lib/constants'
+import React from "react";
+import classNames from "classnames";
+import { Link, useLocation } from "react-router-dom";
+import { HiOutlineLogout } from "react-icons/hi";
+import {
+  DASHBOARD_SIDEBAR_LINKS,
+  DASHBOARD_SIDEBAR_BOTTOM_LINKS,
+} from "../../lib/constants";
 
 const linkClass =
-	'flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm text-base'
+  "flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm text-base";
 
 export default function Sidebar() {
-	return (
-		<div className="bg-blue-600 w-60 p-3 flex flex-col">
-			<div className="flex items-center gap-2 px-1 py-3">
-				<span className="text-white font-bold text-3xl ml-4">Ace Garage</span>
-			</div>
-			<div className="py-8 flex flex-1 flex-col gap-0.5">
-				{DASHBOARD_SIDEBAR_LINKS.map((link) => (
-					<SidebarLink key={link.key} link={link} />
-				))}
-			</div>
-			<div className="flex flex-col gap-0.5 pt-2 border-t border-neutral-700">
-				<div className={classNames(linkClass, 'cursor-pointer text-red-500')}>
-					<span className="text-xl">
-						<HiOutlineLogout />
-					</span>
-					Logout
-				</div>
-			</div>
-		</div>
-	)
+  return (
+    <div className="bg-blue-600 w-60 p-3 flex flex-col">
+      <Link to={"/"} className="flex items-center gap-2 px-1 py-3">
+        <span className="text-white font-bold text-3xl ml-4">Ace Garage</span>
+      </Link>
+      <div className="py-8 flex flex-1 flex-col gap-0.5">
+        {DASHBOARD_SIDEBAR_LINKS.map((link) => (
+          <SidebarLink key={link.key} link={link} />
+        ))}
+      </div>
+      <div className="flex flex-col gap-0.5 pt-2 border-t border-neutral-700">
+        <div className={classNames(linkClass, "cursor-pointer text-red-500")}>
+          <span className="text-xl">
+            <HiOutlineLogout />
+          </span>
+          Logout
+        </div>
+      </div>
+    </div>
+  );
 }
 
 function SidebarLink({ link }) {
-	const { pathname } = useLocation()
+  const { pathname } = useLocation();
 
-	return (
-		<Link
-			to={link.path}
-			className={classNames(pathname === link.path ? 'bg-blue-700 text-white' : 'text-white font-medium', linkClass)}
-		>
-			<span className="text-xl">{link.icon}</span>
-			{link.label}
-		</Link>
-	)
+  return (
+    <Link
+      to={link.path}
+      className={classNames(
+        pathname === link.path
+          ? "bg-blue-700 text-white"
+          : "text-white font-medium",
+        linkClass
+      )}
+    >
+      <span className="text-xl">{link.icon}</span>
+      {link.label}
+    </Link>
+  );
 }
