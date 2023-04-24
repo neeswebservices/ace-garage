3a50376ec808902c8e4daf75f42874d3c04cf684
Sudip Kumar Mahato <1234sudipmahato@gmail.com>
2023-04-25 05:15:50 +0545

    asdf

--- client/src/pages/Admin/pages/FAQ.jsx
+++ client/src/pages/Admin/pages/FAQ.jsx
@@ -30,7 +30,7 @@ const FAQ = () => {
               type="text"
               id="faq-title"
               placeholder="How we work?"
-              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline"
               required
             />
           </div>
